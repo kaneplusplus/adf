b02b9cebe83dfcfdde1905304917b6a7837549ee
Michael Kane <kaneplusplus@gmail.com>
2018-02-19 15:16:26 -0500

    Removed lm.model.frame. Do we need it?

--- R/lm.model.frame.r
+++ R/lm.model.frame.r
@@ -1,13 +1,14 @@
-lm.model.frame <- function(formula, data, subset, weights, na.action, 
-                           contrasts = NULL, offset) {
-    cl <- match.call()
-    mf <- match.call(expand.dots = FALSE)
-    m <- match(c("formula", "data", "subset", "weights", "na.action",
-        "offset"), names(mf), 0L)
-    mf <- mf[c(1L, m)]
-    mf$drop.unused.levels <- FALSE
-    mf[[1L]] <- quote(stats::model.frame)
-    mf <- eval(mf, parent.frame())
-    return(mf)
-}
+# DO WE NEED THIS?
+# lm.model.frame <- function(formula, data, subset, weights, na.action, 
+#                            contrasts = NULL, offset) {
+#     cl <- match.call()
+#     mf <- match.call(expand.dots = FALSE)
+#     m <- match(c("formula", "data", "subset", "weights", "na.action",
+#         "offset"), names(mf), 0L)
+#     mf <- mf[c(1L, m)]
+#     mf$drop.unused.levels <- FALSE
+#     mf[[1L]] <- quote(stats::model.frame)
+#     mf <- eval(mf, parent.frame())
+#     return(mf)
+# }
 
